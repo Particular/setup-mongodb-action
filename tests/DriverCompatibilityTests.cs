4ea3de4a136cc19303af61d84e51583e173f7c60
Andreas Öhlund <andreas.ohlund@particular.net>
2024-03-13 15:45:44 +0100

    Add test project

---
+++ tests/DriverCompatibilityTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Clusters;
+using NUnit.Framework;
+
+[TestFixture]
+class DriverCompatibilityTests
+{
+    [Test]
+    public async Task Should_not_have_duplicate_subscriptions()
+    {
+        var containerConnectionString = Environment.GetEnvironmentVariable("MongoDBConnectionString");
+
+        var client = new MongoClient(containerConnectionString);
+
+        using (var session = client.StartSession())
+        {
+            Assert.AreEqual(client.Cluster.Description.Type, ClusterType.ReplicaSet);
+        }
+    }
+}
