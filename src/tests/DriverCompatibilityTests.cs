73235d46f93a58a488b487e206d6616f1676c00c
Daniel Marbach <danielmarbach@users.noreply.github.com>
2024-08-29 10:20:27 +0200

    Fix remaining warnings and errors

--- src/tests/DriverCompatibilityTests.cs
+++ src/tests/DriverCompatibilityTests.cs
@@ -10,16 +10,16 @@ class DriverCompatibilityTests
     public void Should_report_correct_cluster_type()
     {
         var connectionString = Environment.GetEnvironmentVariable("MongoDBConnectionString");
-        TestContext.WriteLine("ConnectionString: " + connectionString);
+        TestContext.Out.WriteLine("ConnectionString: " + connectionString);
 
         var client = new MongoClient(connectionString);
 
         //do a fake call to make sure that cluster details is fetched
         client.ListDatabases();
 
-        TestContext.WriteLine("State: " + client.Cluster.Description.State);
-        TestContext.WriteLine("Type: " + client.Cluster.Description.Type);
-        TestContext.WriteLine("ReplicaSetName: " + client.Cluster.Settings.ReplicaSetName);
+        TestContext.Out.WriteLine("State: " + client.Cluster.Description.State);
+        TestContext.Out.WriteLine("Type: " + client.Cluster.Description.Type);
+        TestContext.Out.WriteLine("ReplicaSetName: " + client.Cluster.Settings.ReplicaSetName);
 
         Assert.Multiple(() =>
         {
