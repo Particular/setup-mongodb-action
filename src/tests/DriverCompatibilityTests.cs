3eaafb607872a4e0ab5d7c9e96e1cc80070c2053
Daniel Marbach <daniel.marbach@openplace.net>
2024-08-30 03:59:40 +0000

    Migrate NUnit assertions to the constraint model in preparation of the NUnit 4 upgrade (#26)
    * Add Nunit.Analyzer to test projects
    
    * Autoformat of test projects for NUnit2005
    
    * Autoformat of test projects for NUnit2045
    
    * Fix remaining warnings and errors
    
    ---------
    
    Co-authored-by: Daniel Marbach <danielmarbach@users.noreply.github.com>

--- src/tests/DriverCompatibilityTests.cs
+++ src/tests/DriverCompatibilityTests.cs
@@ -10,18 +10,21 @@ class DriverCompatibilityTests
     public void Should_report_correct_cluster_type()
     {
         var connectionString = Environment.GetEnvironmentVariable("MongoDBConnectionString");
-        TestContext.WriteLine("ConnectionString: " + connectionString);
+        TestContext.Out.WriteLine("ConnectionString: " + connectionString);
 
         var client = new MongoClient(connectionString);
 
         //do a fake call to make sure that cluster details is fetched
         client.ListDatabases();
 
-        TestContext.WriteLine("State: " + client.Cluster.Description.State);
-        TestContext.WriteLine("Type: " + client.Cluster.Description.Type);
-        TestContext.WriteLine("ReplicaSetName: " + client.Cluster.Settings.ReplicaSetName);
+        TestContext.Out.WriteLine("State: " + client.Cluster.Description.State);
+        TestContext.Out.WriteLine("Type: " + client.Cluster.Description.Type);
+        TestContext.Out.WriteLine("ReplicaSetName: " + client.Cluster.Settings.ReplicaSetName);
 
-        Assert.AreEqual(ClusterType.ReplicaSet, client.Cluster.Description.Type);
-        Assert.AreEqual("tr0", client.Cluster.Settings.ReplicaSetName);
+        Assert.Multiple(() =>
+        {
+            Assert.That(client.Cluster.Description.Type, Is.EqualTo(ClusterType.ReplicaSet));
+            Assert.That(client.Cluster.Settings.ReplicaSetName, Is.EqualTo("tr0"));
+        });
     }
 }
